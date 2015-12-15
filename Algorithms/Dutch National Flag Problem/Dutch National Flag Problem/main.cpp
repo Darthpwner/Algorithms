aa030c688313a683aa7324db04729bfd5d7ef258
Matthew Allen Lin <Darthpwner@Matthews-MacBook-Pro.local>
2015-12-15 08:36:16 -0800

    Added useful print statements for Dutch Natioanl Flag Problem

--- Algorithms/Dutch National Flag Problem/Dutch National Flag Problem/main.cpp
+++ Algorithms/Dutch National Flag Problem/Dutch National Flag Problem/main.cpp
@@ -15,6 +15,17 @@ void swap(int *a, int *b)
     *b = temp;
 }
 
+/* Utility function to print array arr[] */
+void printArray(int arr[], int arr_size)
+{
+    printf("array after segregation ");
+    int i;
+    for (i = 0; i < arr_size; i++)
+        printf("%d ", arr[i]);
+    printf("\n\n");
+}
+
+//Time Complexity: O(n) - Middle is your index that you run through. In the worst case, arr[middle] is never > key, so you run through all n elements
 void dutchNationalFlag(int arr[], int size, int key) {
     int low = 0;
     int middle = 0;
@@ -32,15 +43,11 @@ void dutchNationalFlag(int arr[], int size, int key) {
             middle++;
         }
     }
-}
-
-/* Utility function to print array arr[] */
-void printArray(int arr[], int arr_size)
-{
-    int i;
-    for (i = 0; i < arr_size; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
+    
+    printf("Low: %d\n", low);
+    printf("Medium: %d\n", middle); //Number of iterations through the array
+    printf("High: %d\n", high);
+    printArray(arr, size);
 }
 
 int main(int argc, const char * argv[]) {
@@ -48,10 +55,15 @@ int main(int argc, const char * argv[]) {
     int arr_size = sizeof(arr)/sizeof(arr[0]);
 //    int i;
     
-    dutchNationalFlag(arr, arr_size, 1);
+    int arr1[] = {0, 0, 0, 1, 1, 1, 2, 2, 2};
+    int arr1_size = sizeof(arr1)/sizeof(arr1[0]);
     
-    printf("array after segregation ");
-    printArray(arr, arr_size);
+    dutchNationalFlag(arr, arr_size, 1);
+    dutchNationalFlag(arr1, arr1_size, 1);
+    
+    //printArray(arr, arr_size);
+    //printArray(arr1, arr1_size);
+    
     
     return 0;
 }
