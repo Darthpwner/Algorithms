b89f487d46bb2d367ee3267f14378f3e8ca6d778
Matthew Allen Lin <Darthpwner@Matthews-MacBook-Pro.local>
2015-12-16 10:15:54 -0800

    Added magic index problem for distinct numbers

--- Algorithms/Dynamic Programming/Dynamic Programming/main.cpp
+++ Algorithms/Dynamic Programming/Dynamic Programming/main.cpp
@@ -10,6 +10,8 @@
 using namespace std;
 
 #define N 1000
+#define magicOdd 7
+#define magicEven 8
 
 int runs = 0;
 
@@ -81,6 +83,23 @@ int robotOnGridProblem(int grid[][N], int x, int y) {
     return (findFactorial(x + y)) / (findFactorial(x) * findFactorial(y));
 }
 
+//9.3
+int magicIndex(int arr[], int size, int start, int end) {
+    runs++;
+    
+    if(end < start || start < 0 || end >= size) {
+        return -1;
+    }
+    int mid = (start + end) / 2;
+    if(arr[mid] == mid) {
+        return mid;
+    } else if(arr[mid] > mid) {
+        return magicIndex(arr, size, start, mid - 1);
+    } else {
+        return magicIndex(arr, size, mid + 1, end);
+    }
+}
+
 int main(int argc, const char * argv[]) {
     //9.1: Upper bound is ~33
     int map[N];
@@ -98,5 +117,16 @@ int main(int argc, const char * argv[]) {
     }
 
     print(robotOnGridProblem(grid, 1, 1));
+    
+    //9.3
+    int magicOddArr[magicOdd] = {-10, -7, 0, 1, 2, 5, 9};
+    int magicOddArrSize = sizeof(magicOddArr)/sizeof(magicOddArr[0]);
+    
+    int magicEvenArr[magicEven] = {-21, 1, 5, 100, 101, 102, 103, 104};
+    int magicEvenArrSize = sizeof(magicEvenArr)/sizeof(magicEvenArr[0]);
+    
+    print(magicIndex(magicOddArr, magicOddArrSize, 0, magicOddArrSize - 1));
+    print(magicIndex(magicEvenArr, magicEvenArrSize, 0, magicEvenArrSize - 1));
+    
     return 0;
 }
